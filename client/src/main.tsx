85b4d777228f01f429f6412c95a3ff5ef7766d7f
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-10 22:09:06 +0500

    feat[fe]: initial router setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import './configs/i18next'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback='Loading...'>
-      <App />
-    </React.Suspense>
+    <BrowserRouter>
+      <React.Suspense fallback='Loading...'>
+        <App />
+      </React.Suspense>
+    </BrowserRouter>
   </React.StrictMode>
 )
