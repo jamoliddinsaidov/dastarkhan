b6270fe26978e63a0b00ab50cf4cbd30b3bef0c3
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-10 12:26:28 +0500

    feat(fe): setup i18next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { MantineUiProvider } from './hoc/MantineUiProvider'
+import { useTranslation } from 'react-i18next'
+import { languages } from './configs/i18next'
 
 export const App = () => {
+  const { t, i18n } = useTranslation()
   return (
     <MantineUiProvider>
-      <div>Client Application</div>
+      <div>
+        {Object.entries(languages).map(([lng, value]) => (
+          <button key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>
+            {value.nativeName}
+          </button>
+        ))}
+      </div>
+
+      <div>{t('greeting')}</div>
     </MantineUiProvider>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import './configs/i18next'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback='Loading...'>
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 )
