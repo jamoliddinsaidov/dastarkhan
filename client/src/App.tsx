3227be4b15e8548128ad12ace9e1392998d3277c
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-11 15:03:06 +0500

    feat[fe]: add Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { MantineUiProvider } from './hoc/MantineUiProvider'
 import { GeneralHeader } from './components/GeneralHeader/GeneralHeader'
-import { HeroWithBackground } from './components/HeroWithBackground/HeroWithBackground'
+import { Home } from './pages/Home/Home'
 
 export const App = () => {
   return (
     <MantineUiProvider>
       <GeneralHeader />
-      <HeroWithBackground />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Navigate to='/home' />} />
+      </Routes>
     </MantineUiProvider>
   )
 }
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeroWithBackground } from '../../components/HeroWithBackground/HeroWithBackground'
+
+export const Home = () => {
+  return (
+    <>
+      <HeroWithBackground />
+    </>
+  )
+}
