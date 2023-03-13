15adf20043df553d30c81a7866f1cde8ba5c858a
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-13 11:32:19 +0500

    feat[be]: add User model

---
+++ server/src/models/User.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+})
+
+export const User = mongoose.model('User', UserSchema)
