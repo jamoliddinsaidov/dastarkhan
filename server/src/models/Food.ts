5a4c32b4e03b72bc767bed880c75a6a9173d3e1d
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-14 23:57:27 +0500

    feat[be]: add Food model

---
+++ server/src/models/Food.ts
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose'
+
+const userSubSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  userId: {
+    type: String,
+    defaultValue: null,
+  },
+})
+
+const FoodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  place: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  review: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  rating: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  foodType: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  serviceType: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  // image: {
+
+  // }
+  user: {
+    type: userSubSchema,
+  },
+})
+
+export const Food = mongoose.model('Food', FoodSchema)
+
+export interface FoodRequestBody {
+  name: string
+  place: string
+  review: string
+  city: string
+  rating: string
+  price: number
+  foodType: string
+  serviceType: string
+  user: {
+    name: string
+    userId?: string
+  }
+}
