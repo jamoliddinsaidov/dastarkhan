849890a4606adb79478e9d0f65e68674749e54eb
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-30 23:03:58 +0500

    fix: modify ContactUs component

--- client/src/components/ContactUs/ContactUs.tsx
+++ client/src/components/ContactUs/ContactUs.tsx
@@ -1,4 +1,4 @@
-import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid, Container } from '@mantine/core'
+import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid, Container, Title, Badge } from '@mantine/core'
 import { useTranslation } from 'react-i18next'
 import { ContactIconsList } from './ContactIcons'
 import { useContactUsStyles } from './ContactUs.style'
@@ -8,36 +8,43 @@ export const ContactUs = () => {
   const { classes } = useContactUsStyles()
 
   return (
-    <Paper pb={8}>
-      <Container className={classes.wrapper}>
-        <div className={classes.contacts}>
-          <Text fz='lg' fw={700} className={classes.title}>
-            {t('contact_information')}
-          </Text>
+    <>
+      <Group position='center'>
+        <Badge variant='filled' size='lg' mt='lg'>
+          {t('contact_us')}
+        </Badge>
+      </Group>
+      <Paper pb={8}>
+        <Container className={classes.wrapper}>
+          <div className={classes.contacts}>
+            <Text fz='lg' fw={700} className={classes.title}>
+              {t('contact_information')}
+            </Text>
 
-          <ContactIconsList />
-        </div>
-
-        <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
-          <Text fz='lg' fw={700} className={classes.title}>
-            {t('get_in_touch')}
-          </Text>
-
-          <div className={classes.fields}>
-            <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-              <TextInput label={t('your_name')} placeholder={t('your_name')!} required />
-              <TextInput label={t('email_addess')} placeholder='joh@gmail.com' required />
-            </SimpleGrid>
-            <TextInput mt='md' label={t('subject')} placeholder={t('subject')!} required />
-            <Textarea mt='md' label={t('your_message')} placeholder={t('relevant_info')!} minRows={3} required />
-            <Group position='right' mt='md'>
-              <Button type='submit' className={classes.control}>
-                {t('send_message')}
-              </Button>
-            </Group>
+            <ContactIconsList />
           </div>
-        </form>
-      </Container>
-    </Paper>
+
+          <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+            <Text fz='lg' fw={700} className={classes.title}>
+              {t('get_in_touch')}
+            </Text>
+
+            <div className={classes.fields}>
+              <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+                <TextInput label={t('your_name')} placeholder={t('your_name')!} required />
+                <TextInput label={t('email_addess')} placeholder='joh@gmail.com' required />
+              </SimpleGrid>
+              <TextInput mt='md' label={t('subject')} placeholder={t('subject')!} required />
+              <Textarea mt='md' label={t('your_message')} placeholder={t('relevant_info')!} minRows={3} required />
+              <Group position='right' mt='md'>
+                <Button type='submit' className={classes.control}>
+                  {t('send_message')}
+                </Button>
+              </Group>
+            </div>
+          </form>
+        </Container>
+      </Paper>
+    </>
   )
 }
